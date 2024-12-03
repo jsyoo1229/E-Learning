819c20d201c6f5c5720c2039250945d9b5bf0edf
Jinsun Yoo <jsyoo1029@gmail.com>
2024-12-04 02:51:22 +0900

    views.py, urls.py 작성

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Course, Lesson
-from .serializers import 
+from .serializers import LessonSerializer, CourseSerializer
 from .filters import CourseFilter
 
+class ClassViewset(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CourseFilter
+
+class LessonViewset(viewsets.ModelViewSet):
+    queryset = Lesson.objects.all()    
+    serializer_class = LessonSerializer
+
+    
